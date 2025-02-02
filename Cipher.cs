af0fa706d9b7d5243c9fc445339bfc84515c3b8f
Andy Collins <anthonius.bruce@gmail.com>
2025-02-02 11:50:11 -0500

    Added the cipher to do the actual work. Started with a create key method. changed the text of the help to no longer support placing the generated key in an environment variable.

---
+++ Cipher.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Security.Cryptography;
+
+namespace Aes256Cipher
+{
+    internal static class Cipher
+    {
+        internal static string CreateNewKey()
+        {
+            using var aes = Aes.Create();
+            aes.GenerateKey();
+            return Convert.ToBase64String(aes.Key);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿if (args.Length == 0)
+﻿using Aes256Cipher;
+
+if (args.Length == 0)
 {
     OutDamnedHelp();
     return;
@@ -71,12 +73,18 @@ if ((encrypt || decrypt) && string.IsNullOrWhiteSpace(keyName))
     return;
 }
 
+if (createKey)
+{
+    var newKey = Cipher.CreateNewKey();
+    Console.WriteLine($"Key: {newKey}");
+}
+
 void OutDamnedHelp()
 {
     Console.WriteLine("Creates a new Aes256 key or encrypts a file or decrypts a file. When creating a new key of a key name is given,\nthe application will replace the environment variable value with the new key. When encrypting or decrypting a\nfile, the application with modify the values found after 'CipherText:'. In the example of\n\"Connection_string\":\"CipherText:<data>\" will encrypt/decrypt just <data>.\n");
     Console.WriteLine("Aes256Cipher.exe create|encrypt|decrypt [--keyname|-k|/k \"key name\"] [--filename|-f|/f \"file path\"] [--help|-h|/h]\n");
     Console.WriteLine("    create|encrypt|decrypt\tThe action to be taken, either create or encrypt or decrypt");
-    Console.WriteLine("    --keyname \"key name\"\tThe name of the environment variable where the key resides, optional\n\t\t\t\t    for create but required for encrypt or decrypt");
+    Console.WriteLine("    --keyname \"key name\"\tThe name of the environment variable where the key resides, not used\n\t\t\t\t    for create but required for encrypt or decrypt");
     Console.WriteLine("    --filename \"file name\"\tThe file name and path with the data to be encrypted or decrypted");
     Console.WriteLine("    --help\t\t\tThis help");
 }
