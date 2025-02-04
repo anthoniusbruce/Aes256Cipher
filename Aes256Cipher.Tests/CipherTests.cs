4759ce7bb321101d5a43d570ac8182667ef73db6
Andy Collins <anthonius.bruce@gmail.com>
2025-02-04 15:25:30 -0500

    Added tests and added encrypt and decrypt tests and methods

---
+++ Aes256Cipher.Tests/CipherTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Aes256Cipher.Tests
+{
+    [TestClass]
+    public sealed class CipherTests
+    {
+        static readonly byte[] KEY = Convert.FromBase64String("r02fdzOj9ksNZAQk7KWHuRfMishwSXZarrTlzWRw1xc=");
+        private string Decipher(string value)
+        {
+            var ivAndCipherText = Convert.FromBase64String(value);
+            using var aes = Aes.Create();
+            var ivLength = aes.BlockSize / 8;
+            aes.IV = ivAndCipherText.Take(ivLength).ToArray();
+            aes.Key = KEY;
+            using var cipher = aes.CreateDecryptor();
+            var cipherText = ivAndCipherText.Skip(ivLength).ToArray();
+            var text = cipher.TransformFinalBlock(cipherText, 0, cipherText.Length);
+            return Encoding.UTF8.GetString(text);
+        }
+
+
+        [TestMethod]
+        public void CreateNewKey_ReturnsNonEmptyStringEndingwithEqual()
+        {
+            var key = Cipher.CreateNewKey();
+
+            Assert.IsTrue(key.EndsWith("="));
+        }
+
+        [TestMethod]
+        public void Encrypt_Text_ReturnsEncryptedText()
+        {
+            var encryptMe = "The quick fox jumped over the lazy hound";
+
+            var actual = Cipher.Encrypt(encryptMe, KEY);
+
+            Assert.AreEqual(encryptMe, Decipher(actual));
+        }
+
+        [TestMethod]
+        public void Encrypt_EmptyText_ReturnsEncryptedText()
+        {
+            var encryptMe = string.Empty;
+
+            var actual = Cipher.Encrypt(encryptMe, KEY);
+
+            Assert.AreEqual(encryptMe, Decipher(actual));
+        }
+
+        [TestMethod]
+        public void Decrypt_Text_ReturnsDecryptedText()
+        {
+            var decryptMe = "Tk2zXsG2IBs2dnv7IWgzyFkiRimKp73G1iIwTt9tYqQVXPAcpM06wEXHRVeTgDE1BnkKE9mDHi2OspLVwvPT8g==";
+            var expected = "The quick fox jumped over the lazy hound";
+
+            var actual = Cipher.Decrypt(decryptMe, KEY);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("")]
+        [DataRow("123456789abcdef0")]
+        public void Decrypt_EmptyText_EmptyText(string decryptMe)
+        {
+            var expected = string.Empty;
+
+            var actual = Cipher.Decrypt(decryptMe, KEY);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReplaceCipherText_ReturnsToUpperText()
+        {
+            var text = @"{""tag"":""CipherText:upper me""}";
+            var modifier = (string input, byte[] x) =>
+            {
+                return input.ToUpper();
+            };
+            var expected = @"{""tag"":""CipherText:UPPER ME""}";
+
+            var actual = Cipher.ReplaceCipherText(text, Array.Empty<byte>(), modifier);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(@"{""tag"":""CipherText:""}")]
+        [DataRow(@"{""tag"":""no tag""}")]
+        [DataRow(@"{""tag"":""""}")]
+        [DataRow("")]
+        public void ReplaceEmptyCipherText_ReturnsOriginalText(string text)
+        {
+            var modifier = (string input, byte[] x) =>
+            {
+                return input.ToUpper();
+            };
+            var expected = text;
+
+            var actual = Cipher.ReplaceCipherText(text, Array.Empty<byte>(), modifier);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Aes256Cipher/Cipher.cs
@@ -0,0 +1,86 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aes256Cipher
+{
+    public static class Cipher
+    {
+        public static string CreateNewKey()
+        {
+            using var aes = Aes.Create();
+            aes.GenerateKey();
+            return Convert.ToBase64String(aes.Key);
+        }
+
+        public static void EncryptSecuredJsonFields(string fileName, string key)
+        {
+            var jsonFile = new FileInfo(fileName);
+            var json =  File.ReadAllText(jsonFile.FullName);
+            var byteKey = Convert.FromBase64String(key);
+            var result = ReplaceCipherText(json, byteKey, Encrypt);
+
+            File.WriteAllText(jsonFile.FullName, result);
+        }
+
+        public static string Encrypt(string text, byte[] key)
+        {
+            using var aes = Aes.Create();
+            aes.Key = key;
+            aes.GenerateIV();
+            using var cipher = aes.CreateEncryptor();
+            var byteText = Encoding.UTF8.GetBytes(text);
+            var cipherText = cipher.TransformFinalBlock(byteText, 0, byteText.Length);
+            return Convert.ToBase64String(aes.IV.Concat(cipherText).ToArray());
+        }
+
+        public static void DecryptSecuredJsonFields(string fileName, string key)
+        {
+            var jsonFile = new FileInfo(fileName);
+            var json = File.ReadAllText(jsonFile.FullName);
+            var byteKey = Convert.FromBase64String(key);
+            var result = ReplaceCipherText(json, byteKey, Decrypt);
+
+            File.WriteAllText(jsonFile.FullName, result);
+        }
+
+        public static string Decrypt(string text, byte[] key)
+        {
+            var ivAndCipherText = Convert.FromBase64String(text);
+            using var aes = Aes.Create();
+            var ivLength = aes.BlockSize / 8;
+            if (ivLength > ivAndCipherText.Length)
+            {
+                return string.Empty;
+            }
+            aes.IV = ivAndCipherText.Take(ivLength).ToArray();
+            aes.Key = key;
+            using var cipher = aes.CreateDecryptor();
+            var cipherText = ivAndCipherText.Skip(ivLength).ToArray();
+            var result = cipher.TransformFinalBlock(cipherText, 0, cipherText.Length);
+            return Encoding.UTF8.GetString(result);
+        }
+
+        public static string ReplaceCipherText(string json, byte[] byteData, Func<string, byte[], string> modifier)
+        {
+            var result = Regex.Replace(json,
+                @"""CipherText:(?<Text>[^""]+)""",
+                m =>
+                {
+                    var text = m.Groups["Text"].Value;
+                    try
+                    {
+                        var cipherText = modifier(text, byteData);
+                        return $@"""CipherText:{cipherText}""";
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($@"Failed ""{text}""");
+                        return m.Value;
+                    }
+                });
+            return result;
+        }
+    }
+}
